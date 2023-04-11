31bd1b00f366c459c5fdf6981e881b4624059941
Nguyen Phong <nguyenphong.mobile.engineer@gmail.com>
2023-04-11 10:14:11 +0700

    change version name

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "ButtonStyles",
-      targets: ["ButtonStyles"]),
+      name: "SwiftUIButtonStyles",
+      targets: ["SwiftUIButtonStyles"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -25,10 +25,10 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "ButtonStyles",
+      name: "SwiftUIButtonStyles",
       dependencies: []),
     .testTarget(
-      name: "ButtonStylesTests",
-      dependencies: ["ButtonStyles"]),
+      name: "SwiftUIButtonStylesTests",
+      dependencies: ["SwiftUIButtonStyles"]),
   ]
 )
